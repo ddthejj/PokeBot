80bb8f26c2a31da927ca278b80eb3076dd9d119c
ddthejj <cooleaddento@gmail.com>
2021-09-10 17:43:17 -0400

    working on server stuff

--- PokeBot/Users/Server.cpp
+++ PokeBot/Users/Server.cpp
@@ -2,8 +2,12 @@
 
 #include "User.h"
 
+#include <string>
+
 #include <iostream>
 #include <fstream>
+#include <experimental/filesystem>
+#include <sys/stat.h>
 
 DiscordServer::DiscordServer(int64_t In_ID)
 {
@@ -14,11 +18,25 @@ DiscordServer::DiscordServer(int64_t In_ID)
 
 	std::string serverFilePath = std::string("../../../Content/Servers/") + buffer;
 
-	std::ifstream file(serverFilePath, std::ios::in);
+	struct stat info;
+	if (stat(serverFilePath.c_str(), &info) != 0)
+	{
 
-	if (!file.is_open())
-		return;
+	}
+	else if (info.st_mode & S_IFDIR)
+	{
+		// existing server		
+		std::vector<std::string> lines;
+		std::string line;
 
-
-
+		while (std::getline(f, line))
+		{
+			lines.push_back(line);
+		}
+	}
+	else
+	{
+		// new server
+		std::experimental::filesystem::create_directories(serverFilePath);
+	}
 }
